ac34359f2eeb121565ec814baacd52a1373ead6c
Peter Ajtai <peter.ajtai@soliddigital.com>
2020-06-15 22:22:27 -0700

    Adding GA

--- source/browser-wars/build.js
+++ source/browser-wars/build.js
@@ -5,6 +5,15 @@ const file = `
 <!DOCTYPE html>
 <html>
 <head>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-4145174-19"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'UA-4145174-19');
+    </script>
     <!-- Load c3.css -->
     <link href="/browser-wars/c3-0.7.15/c3.css" rel="stylesheet">
 
