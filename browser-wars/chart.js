e3dfe2422880bee1ec18149009a5aac373075da4
Peter Ajtai <peter.ajtai@soliddigital.com>
2020-07-28 00:44:08 -0700

    Site updated: 2020-07-28 00:44:08

--- browser-wars/chart.js
+++ browser-wars/chart.js
@@ -1,112 +1,56 @@
 (function(c3, d3, getData) {
     var h = window.innerHeight;
     var data = getData();
-    var size = h/2;
-    var chart1 = c3.generate({
-        bindto: '#chart1',
-        responsive: true,
-        size: {
-            height: size
-        },
-        donut: {
-            title: 'Browser Usage'
-        },
-        data: {
-            columns: [
-                ['Chrome', 1.38],
-                ['IE', 65.41],
-                ['Firefox', 27.03],
-                ['Safari', 2.57],
-                ['Opera', 2.92],
-                ['Edge Legacy', 0],
-                ['Other', 0],
-            ],
-            type : 'donut',
-            order: null,
-            transition: {
-                duration: 1000
-            },
-            tooltip: {
-                show: false
-            }
-        },
-    });
 
-    var lineChartConfigs = {
-        bindto: '#chart2',
-        size: {
-            height: size
-        },
-        data: {
-            x: 'x',
-            columns: [
-                ['x'],
-                ['Chrome'],
-                ['IE'],
-                ['Firefox'],
-                ['Safari'],
-                ['Opera'],
-                ['Edge Legacy'],
-                ['Other'],
-            ]
-        },
-        axis: {
-            x: {
-                type: 'timeseries',
-                tick: {
-                    format: '%Y-%m-%d'
-                }
-            }
-        },
-        legend: {
-            show: false
-        },
-        point: {
-            show: false
-        },
-        tooltip: {
-            show: true
-        }
-    };
+    var datavars = [22, 84, 29, 3];
+    var colors = ['red','orange','green','blue'];
 
-    data.forEach(function(datum) {
-        lineChartConfigs.data.columns[0].push(datum.title + '-1');
-        for (var i=0; i<7; ++i) {
-            lineChartConfigs.data.columns[i + 1].push(datum.data[i][1]);
-        }
-    });
-    var chart2 = c3.generate(lineChartConfigs);
-    var index = 0;
-    chart2.tooltip.show({ x: 1 });
+    var svg = d3.select('#chart1')
+        .append('svg')
+        .attr('width', 500)
+        .attr('height', 100);
 
-    var interval = setInterval(update, 1000);
-    chart1.internal.config.interaction_enabled = false;
-    chart1.internal.config.onmouseover = function() {};
-    chart1.internal.config.onmouseout = function() {};
-    chart1.internal.config.data_onmouseover = function() {};
-    chart1.internal.config.click = function() {};
-    chart1.internal.config.hover = function() {};
-    chart1.internal.expandArc = function() {};
-    chart1.internal.showTooltip = function() {};
+    svg.selectAll('rect')
+        .data(datavars)
+        .enter()
+        .append('rect')
+        .attr('width', function(d){
+            return d;})
+        .attr('x',function(d, i){
+            return sum(datavars, 0, i); })
+        .attr('fill', function(d, i){ return colors[i]; })
+        .attr('y',0)
+        .attr('height', 30);
 
-    d3
-        .select('#chart2')
-        .on('mouseover', function() {
-            clearInterval(interval);
-        })
-        .on('mouseout', function() {
-            interval = setInterval(update, 1000);
-        });
-
-    function update () {
-        if (index > data.length - 1) { index = 0; }
-
-        var datum = data[index];
-        d3.select('#chart1 .c3-chart-arcs-title').node().innerHTML = datum.title;
-        chart1.load({ columns: datum.data });
-        chart2.tooltip.show({ data: { x: new Date(datum.title + '-01')}});
-        document.getElementsByClassName('c3-tooltip')[0].style.visibility = 'hidden';
-        index += 3;
-
+    function sum(array, start, end) {
+        var total = 0;
+        for(var i=start; i<end; i++) total += array[i];
+        return total;
     }
+
+    function getDocHeight() {
+        var D = document;
+        return Math.max(
+            D.body.scrollHeight, D.documentElement.scrollHeight,
+            D.body.offsetHeight, D.documentElement.offsetHeight,
+            D.body.clientHeight, D.documentElement.clientHeight
+        );
+    }
+
+    var docheight = getDocHeight();
+    function amountscrolled(){
+        var winheight= window.innerHeight || (document.documentElement || document.body).clientHeight;
+        var docheight = getDocHeight();
+        var scrollTop = window.pageYOffset || (document.documentElement || document.body.parentNode || document.body).scrollTop;
+        var trackLength = docheight - winheight;
+         // gets percentage scrolled (ie: 80 or NaN if tracklength == 0)
+        var pctScrolled = Math.floor(scrollTop/trackLength * 100);
+        console.log(pctScrolled + '% scrolled');
+        var size = data.length;
+        var index = Math.floor(size * pctScrolled / 100);
+    }
+
+    window.addEventListener("scroll", amountscrolled, false);
 }(window.c3, window.d3, window.getData));
+
+
