85b02fd06c60b7053085cffe813aeb44f855d564
Peter Ajtai <peter.ajtai@soliddigital.com>
2023-10-31 19:08:42 -0700

    Slider ugly but working

--- source/central-tendency/components/chart-one.js
+++ source/central-tendency/components/chart-one.js
@@ -13,30 +13,83 @@ export class ChartOne extends HTMLElement {
 
     constructor() {
         super();
-        this.numbers = this.createNumbers(0.5);
         const slider = document.getElementById('chart-one-slider');
         slider.addEventListener('percent-slider', event => {
             this.renderNumbers(this.createNumbers(event.percent));
         });
 
-        this.renderNumbers(this.numbers);
+        this.renderNumbers(this.createNumbers(0.5));
     }
 
+    /**
+     * chart-one
+     *  .numbers-list
+     * @param numbers
+     */
     renderNumbers(numbers) {
-        let contents = this.firstChild;
-        if (this.firstChild) {
-            this.removeChild(contents);
+        this.renderChart(numbers);
+        this.renderList(numbers);
+    }
+
+    renderChart(numbers) {
+        let chart = this.getElementsByClassName('chart');
+        if (chart.length) {
+            chart = chart[0];
+        } else {
+            chart = document.createElement('canvas');
+            chart.classList.add('chart');
+            chart.style.backgroundColor = '#FFF';
+            this.appendChild(chart);
         }
-        contents = document.createElement('div');
-        contents.classList.add('chart-one_contents');
+        let bars = [];
+        numbers.forEach((number, bin) => {
+            bars[bin] = bars[bin] || [];
+            bars[bin] += number;
+        });
+        const ctx = chart.getContext('2d');
+        const canvasWidth = chart.width;
+        const canvasHeight = chart.height;
+        this.clearChart(ctx, canvasWidth, canvasHeight);
+
+        bars.forEach((kids, bags) => {
+            this.drawBar(ctx, canvasWidth, canvasHeight, bags, kids);
+        });
+    }
+
+    clearChart(ctx, width, height) {
+        ctx.clearRect(0, 0, width, height);
+    }
+
+    drawBar(ctx, canvasWidth, canvasHeight, x, height) {
+        const barWidth = 20;
+        const scale = 25;
+        ctx.fillStyle = 'blue';
+        ctx.fillRect(barWidth * x + (x - 1) * 2, canvasHeight - scale * height, barWidth, scale * height);
+    }
+
+    renderList(numbers) {
+        let numbersList = this.getElementsByClassName('numbers-list');
+        if (numbersList.length) {
+            numbersList = numbersList[0];
+        } else {
+            numbersList = document.createElement('div');
+            numbersList.classList.add('numbers-list');
+            this.appendChild(numbersList);
+        }
+        let numbersListContents = numbersList.firstChild;
+        if (numbersList.firstChild) {
+            numbersList.removeChild(numbersListContents);
+        }
+        numbersListContents = document.createElement('div');
+        numbersListContents.classList.add('chart-one_contents');
         numbers.forEach((number, bin) => {
             for (let i = 1; i <= number; ++i) {
                 let count = document.createElement('pre');
                 count.innerHTML = `&nbsp;${bin}&nbsp;`;
-                contents.appendChild(count);
+                numbersListContents.appendChild(count);
             }
         });
-        this.appendChild(contents);
+       numbersList.appendChild(numbersListContents);
     }
     createNumbers(percent) {
         const min = 1;
